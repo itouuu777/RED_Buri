30295148bdc024b44a2d3cd3b7a19e51697c3508
itouuu777 <135014775+itouuu777@users.noreply.github.com>
2026-01-30 20:08:24 +0900

    Create ENA+.c

---
+++ stepping_motor/ENA+.c
@@ -0,0 +1,22 @@
+	  	  //減速比15:1で200に15をかけて3000パルス/一回点
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_RESET);//動く
+        HAL_Delay(50);
+	    //HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET);//右
+	    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);//左
+	    HAL_Delay(200);
+	    for(int i=0; i < 200; i++){
+
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_SET);
+			HAL_Delay(1);
+
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_RESET);
+			HAL_Delay(1);
+
+	    }
+	    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_SET);//保持
+
+	    HAL_Delay(100000);
+
+	    while(1){
+
+	    }
